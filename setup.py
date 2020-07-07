985339a2580136ff0c7329990199cf03a9b643b7
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2020-07-07 22:43:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f: 
     long_description = f.read()
 
-setup(
+setup( 
     name='summrizer',
 
     version='1.0.0.dev1',
